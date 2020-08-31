95820946b2e9c8448d04ac96b6adf5832d6dd67a
yuki <yk.yuki21@gmail.com>
2020-08-31 23:55:20 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import { Button } from '@material-ui/core'
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`
+
+export default function Home() {
+  return (
+    <>
+      <Title>My page</Title>
+      <Button variant="contained" color="primary">button</Button>
+    </>
+  )
+}
