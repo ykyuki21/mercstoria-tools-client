39e5a9c1dffbb2a27f06b5fddb3388165acf67c3
yuki <yk.yuki21@gmail.com>
2020-09-07 03:34:39 +0900

    Add global navigation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import GlobalNavi from '../components/common/globalnavi'
+import 'sanitize.css'
 
 const theme = {}
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalNavi />
       <Component {...pageProps} />
     </ThemeProvider>
   )
