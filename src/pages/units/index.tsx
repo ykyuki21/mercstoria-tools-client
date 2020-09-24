21de855f915f00032e2c84eeb5ddc79112382ecc
yuki <yk.yuki21@gmail.com>
2020-09-24 17:33:11 +0900

    Fix units page

--- src/pages/units/index.tsx
+++ src/pages/units/index.tsx
@@ -1,29 +1,103 @@
-import { NextPage, GetStaticProps } from 'next'
-import type { Unit } from '~/interfaces/unit'
+import React from "react";
+import { NextPage, GetStaticProps } from "next";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Checkbox,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
+import { Favorite, FavoriteBorder } from "@material-ui/icons";
+import type { Unit } from "~/interfaces/unit";
+
+const useStyles = makeStyles({
+  container: {
+    padding: "20px 0",
+  },
+  table: {
+    minWidth: 375,
+  },
+});
 
 type Props = {
-  units: Unit[]
-}
+  units: Unit[];
+};
 
-const Units: NextPage<Props> = props => {
+const Units: NextPage<Props> = (props) => {
+  const classes = useStyles();
+
   return (
-    <ul>
-      {props.units.map((unit) => (
-        <li>{unit.id}: {unit.name} </li>
-      ))}
-    </ul>
-  )
-}
+    <Container className={classes.container}>
+      <TableContainer component={Paper}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" padding="checkbox">
+                所持
+              </TableCell>
+              <TableCell align="center" padding="checkbox">
+                武器
+              </TableCell>
+              <TableCell align="center" padding="checkbox">
+                レア
+              </TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">DPS</TableCell>
+              <TableCell align="right">Rune</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.units.map((unit) => (
+              <TableRow key={unit.id}>
+                <TableCell align="center" padding="checkbox">
+                  <Checkbox
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite />}
+                  />
+                </TableCell>
+                <TableCell align="center" padding="checkbox">
+                  {unit.weapon}
+                </TableCell>
+                <TableCell align="center" padding="checkbox">
+                  {unit.rarity}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {unit.name}
+                </TableCell>
+                <TableCell align="right">{unit.element}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
+
+type UnitResponse = {
+  units: Unit[];
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch('http://127.0.0.1:4010/units')
-  const units: Unit[] = await res.json()
+  const response = await fetch("http://localhost:3000/units").then<
+    UnitResponse
+  >((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
 
   return {
     props: {
-      units,
+      units: response.units,
     },
-  }
-}
+  };
+};
 
-export default Units
+export default Units;
